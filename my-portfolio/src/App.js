185ffbb9ccfbb010b4ce796215c0189f4d4275e1
yulichka007 <kn23-yu.motrych@nubip.edu.ua>
2025-05-15 15:46:47 +0300

    Виправлено маршрути для GitHub Pages

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutMe from './pages/AboutMe';
